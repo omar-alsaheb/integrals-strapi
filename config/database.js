78b37003253ec62ba046dc65ecfefbf131beeb02
Omar Altamimi <omarT@leading-point.com>
2024-08-15 18:57:29 +0300

    update db

--- config/database.js
+++ config/database.js
@@ -4,13 +4,14 @@ module.exports = ({ env }) => {
   const connections = {
     postgres: {
       connection: {
-        host: env('DATABASE_HOST', '192.168.7.82'),
+        host: env('DATABASE_HOST', 'dpg-cqv21888fa8c73fe11j0-a.frankfurt-postgres.render.com'),
         port: env.int('DATABASE_PORT', 5432),
-        database: env('DATABASE_NAME', 'integralsDB'),
-        user: env('DATABASE_USERNAME', 'postgres'),
-        password: env('DATABASE_PASSWORD', 'netcell_dev'),
+        database: env('DATABASE_NAME', 'integrals_strapi_db'),
+        user: env('DATABASE_USERNAME', 'integrals_strapi_db_user'),
+        password: env('DATABASE_PASSWORD', 'T4OVJ0CDZgwM3wKG1b0WbR46idF9TRtH'),
+        ssl: env.bool('DATABASE_SSL', false),
       },
-      debug: false,
+      debug: true,
     }
   };
 
